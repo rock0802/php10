4870cf6d0edb490a8a457a25e8b2148df94a1170
EC2 Default User <ec2-user@ip-172-31-14-11.us-east-2.compute.internal>
2020-10-17 08:09:51 +0000

    課題１４−１

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\News;
+
 class NewsController extends Controller
 {
  public function add (){
@@ -12,7 +14,26 @@ class NewsController extends Controller
  }
  
  public function create(Request $request)
-  {
+  {   
+      $this->validate($request,News::$rules);
+      
+      $news = new News;
+      $form = $request->all();
+      
+      if(isset($form['image'])){
+       $path = $request->file('image')->store('public/image');
+       $news->image_path = basename('$path');
+      }else{
+       $news->image_path = null;
+      }
+      //フォームから送信されてきた下記削除
+      unset($form['token']);
+      unset($form['image']);
+      
+      //DBに保存
+      $news->fill($form);
+      $news->save();
+      
       // admin/news/createにリダイレクトする
       return redirect('admin/news/create');
   }  
--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,14 +5,35 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     public function add ()
     {
         return view ('admin.profile.create');
     }
-    public function create ()
+    public function create (Request $request)
     {
+        $this->validate($request,Profile::$rules);
+      
+      $news = new Profiles;
+      $form = $request->all();
+      
+      if(isset($form['image'])){
+       $path = $request->file('image')->store('public/image');
+       $news->image_path = basename('$path');
+      }else{
+       $news->image_path = null;
+      }
+      //フォームから送信されてきた下記削除
+      unset($form['token']);
+      unset($form['image']);
+      
+      //DBに保存
+      $news->fill($form);
+      $news->save();
+        
         return redirect('admin/profile/create');
     }
     public function edit ()
