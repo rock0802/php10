063697462d409cd50aaa6edec7a1454c0884d607
EC2 Default User <ec2-user@ip-172-31-14-11.us-east-2.compute.internal>
2020-10-29 08:40:11 +0000

    17

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -7,34 +7,69 @@ use App\Http\Controllers\Controller;
 
 use App\News;
 
+
 class NewsController extends Controller
 {
- public function add (){
-     return view('admin.news.create');
- }
- 
- public function create(Request $request)
-  {   
-      $this->validate($request,News::$rules);
-      
+  public function add()
+  {
+      return view('admin.news.create');
+  }
+  public function create(Request $request)
+  {
+      // Varidationを行う
+      $this->validate($request, News::$rules);
       $news = new News;
       $form = $request->all();
-      
-      if(isset($form['image'])){
-       $path = $request->file('image')->store('public/image');
-       $news->image_path = basename('$path');
-      }else{
-       $news->image_path = null;
+      // formに画像があれば、保存する
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+      } else {
+          $news->image_path = null;
       }
-      //フォームから送信されてきた下記削除
-      unset($form['token']);
+      unset($form['_token']);
       unset($form['image']);
-      
-      //DBに保存
+      // データベースに保存する
       $news->fill($form);
       $news->save();
-      
-      // admin/news/createにリダイレクトする
       return redirect('admin/news/create');
+  }
+  public function index(Request $request)
+  {
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          $posts = News::where('title', $cond_title)->get();
+      } else {
+          $posts = News::all();
+      }
+      return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
+  public function edit(Request $request)
+  {
+      // News Modelからデータを取得する
+      $news = News::find($request->id);
+      return view('admin.news.edit', ['news_form' => $news]);
+  }
+  public function update(Request $request)
+  {
+      // Validationをかける
+      $this->validate($request, News::$rules);
+      // News Modelからデータを取得する
+      $news = News::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $news_form = $request->all();
+      unset($news_form['_token']);
+      // 該当するデータを上書きして保存する
+      $news->fill($news_form)->save();
+      return redirect('admin/news/');
+  }
+  // 以下を追記　　
+  public function delete(Request $request)
+  {
+      // 該当するNews Modelを取得
+      $news = News::find($request->id);
+      // 削除する
+      $news->delete();
+      return redirect('admin/news/');
   }  
 }
