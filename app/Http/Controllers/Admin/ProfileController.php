659ab887ec0bf142a067ffccb98ebd102a592d84
EC2 Default User <ec2-user@ip-172-31-14-11.us-east-2.compute.internal>
2020-10-29 13:47:00 +0000

    17-2

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -17,7 +17,7 @@ class ProfileController extends Controller
     {
         $this->validate($request,Profile::$rules);
       
-      $news = new Profiles;
+      $news = new Profile;
       $form = $request->all();
       
       if(isset($form['image'])){
@@ -36,12 +36,31 @@ class ProfileController extends Controller
         
         return redirect('admin/profile/create');
     }
-    public function edit ()
+    public function edit (Request $request)
     {
+       // Profile Modelからデータを取得する
+        $news = Profile::find($request->id);
         return view ('admin.profile.edit');
     }
-    public function update()
+    public function update(Request $request)
     {
+      // Validationをかける
+      $this->validate($request, Profile::$rules);
+      // Profile Modelからデータを取得する
+      $news = Profile::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $news_form = $request->all();
+      unset($news_form['_token']);
+      // 該当するデータを上書きして保存する
+      $news->fill($news_form)->save();
         return redirect('admin/profile/edit');
+    }
+    public function delete(Request $request)
+    {
+      // 該当するProfile Modelを取得
+      $news = Profile::find($request->id);
+      // 削除する
+      $news->delete();
+      return redirect('admin/profile/');
     }
 }
